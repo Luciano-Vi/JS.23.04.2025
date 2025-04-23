8653290034f6fe5ab4d2bbd2c2a2a8fe642900f5
luci <vluciano@gmail.com>
2025-04-23 17:05:30 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,24 @@
+document.write("<h1>Hola JavaScript </h1>")
+
+// Comentarios de una linea
+
+/* comentario de Bloque */
+
+let nombre="carlos"
+
+//Array
+
+let datos=["Ana","Pablo","Carlos"]
+
+let edades=[14,34,22]
+
+let objetoDatos={
+    nombre:"Carlos",
+    edad:25,
+    cuenta:"alumno",
+    notas:[6.8,6.2,7.0]
+}
+
+console.log(datos)
+console.log(edades)
+console.log(objetoDatos)
